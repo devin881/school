e3b7bbbf1e66c4e6087758890f8c67a3e0f2702e
DC <DC@mail.com>
2026-01-28 14:51:36 -0500

    msg

--- test.c
+++ test.c
@@ -33,32 +33,3 @@ int main()
 }#include <stdio.h>
 int main()
 
-{
-    int num[10]
-    for(int i = 0; i < 10; i++)
-
-{
-    printf("%d\n");
-}
-
-{
-    int num [20]
-    for(int i = 0; i < 20; i++)
-    {
-        printf("%d\n, i");
-    }
-}
-{
-    int num [40]
-    for(int i = 0; i < 40;)
-    {
-        printf("%d\n, i")
-    }
-    {
-        int num [303]
-        for(int i = 0; i < 303;)
-        {
-            printf("%d\n, i")
-        }
-    }
-}
