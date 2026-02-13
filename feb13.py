ad16ad49e194184e0b188d0627886ccdd3dc0516
DC <DC@mail.com>
2026-02-13 14:39:53 -0500

    devin

---
+++ feb13.py
@@ -0,0 +1,17 @@
+print("Hello,")
+age = "16"
+print(f"{age}")
+print("8+3")
+print("8-3")
+print("8*3")
+print("8/3")
+print("8//b")
+print("8%3")
+print(4.5**2 * 3.14)
+print(3 + 4 * 2)
+first_name = "Ada"
+last_name= "Lovelace"
+print(f"{first_name} {last_name}")
+message 
+
+
